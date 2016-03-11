374851fb5c2fdfaed1063827ccce4d2cb31a5006
Yanet Morales <yanet.moralesr@gmail.com>
2016-03-11 15:38:46 -0500

    Logs activity list

--- src/main/java/hds/aplications/com/pesobit/LogListActivity.java
+++ src/main/java/hds/aplications/com/pesobit/LogListActivity.java
@@ -1,22 +1,48 @@
 package hds.aplications.com.pesobit;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class NotificationsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+import hds.aplications.com.pesobit.adapters.LogAdapter;
+
+public class LogListActivity extends Activity {
+
+    private ListView listView;
+    private TextView emptyList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notifications);
+        setContentView(R.layout.activity_log_list);
+
+        this.listView = (ListView) findViewById(R.id.list);
+        this.emptyList = (TextView) findViewById(R.id.emptyList);
+
+        List<Log> items = new ArrayList();
+
+        if(items.size() > 0) {
+            // Sets the data behind this ListView
+            this.listView.setAdapter(new LogAdapter(this, items));
+            this.emptyList.setVisibility(View.INVISIBLE);
+        }
+        else{
+            this.emptyList.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_notifications, menu);
+        getMenuInflater().inflate(R.menu.menu_log_list, menu);
         return true;
     }
 
---
+++ src/main/java/hds/aplications/com/pesobit/adapters/LogAdapter.java
@@ -0,0 +1,69 @@
+package hds.aplications.com.pesobit.adapters;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by YANET on 11/03/2016.
+ */
+public class LogAdapter extends BaseAdapter {
+    private Context context;
+    private List<Log> items;
+
+    public LogAdapter(Context context, List<Log> items){
+        this.context= context;
+        this.items = items;
+    }
+
+    @Override
+    public int getCount() {
+        return this.items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return this.items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return  (((Log)this.getItem(position)).getId() != null) ?((Log)this.getItem(position)).getId(): ((Log)this.getItem(position)).getIdRef();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View rowView = convertView;
+
+        if (convertView == null) {
+            // Create a new view into the list.
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            rowView = inflater.inflate(R.layout.reservation_item, parent, false);
+        }
+
+        // Set data into the view.
+        TextView textTitle = (TextView) rowView.findViewById(R.id.textTitle);
+        TextView textCheckout = (TextView) rowView.findViewById(R.id.textCheckout);
+        TextView textCheckin = (TextView) rowView.findViewById(R.id.textCheckin);
+        TextView roomsTotal = (TextView) rowView.findViewById(R.id.roomsTotal);
+
+        Resources res = this.context.getResources();
+        String roomString = this.context.getResources().getQuantityString(R.plurals.rooms, 2, 2);
+
+        Log item = this.items.get(position);
+        textTitle.setText(item.getAccommodation().getName());
+        textCheckout.setText("Apr 16, 2016");
+        textCheckin.setText("Apr 20, 2016");
+        roomsTotal.setText(roomString);
+
+        return rowView;
+    }
+}
+
